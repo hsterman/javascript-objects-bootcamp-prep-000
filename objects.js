e5d4f6d31073b0809fa6fa854fc419a8472862b8
heather sterman <heather.sterman531@gmail.com>
2018-09-18 19:09:34 +0000

    automatically backed up by learn

--- objects.js
+++
@@ -1,7 +0,0 @@
-var playlist = { U2: "Sunday Bloody Sunday", Rush: "Limelight", Metallica: "One", NIN: 'Closer'};
-
-Object.assign({}, playlist, { U2: ['Sunday Bloody Sunday','New Years Day']} )
-
-function updatePlayList() {}
-
-function removeFromPlaylist() {}
